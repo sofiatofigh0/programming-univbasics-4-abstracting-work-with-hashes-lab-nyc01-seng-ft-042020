81fdc6a827f6def4e565d20a8036c681f6a5853b
sofia tofigh <sst2140@columbia.edu>
2020-04-03 02:23:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,12 +1,12 @@
 def my_hash_creator(key, value)
   hash = {key => value}
-  return hash
+   hash
   # return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
 
- return hash[key]
+  hash[key]
   # return the correct value using the hash and key parameters
 end
 
